6367dda94ef9c6180fc91430535efdf8b51d8a02
Rojan <shrestharojan79@gmail.com>
2024-01-18 09:06:49 +0545

    cart overlays managed

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = props => {
     <Fragment>
       <header className={classes.header}>
         <h1>Luscious Meal</h1>
-        <HeaderCartButton/>
+        <HeaderCartButton onClick={props.onShowCart}/>
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="Table Bhari Mitho khana" />
